f7aeec3d98ce138d49e9c40b7d94898a9a37f538
Gavin_Chen <77559123+CrypticT1tan@users.noreply.github.com>
2025-07-31 22:35:43 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,6 @@
+from color_palette import ColorPalette
+
+if __name__ == "__main__":
+    color_palette = ColorPalette()
+    # Prevent the window from closing immediately upon reaching the end of this file
+    color_palette.window.mainloop()
